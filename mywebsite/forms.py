8c42617a410d125b2435a18942e369f4dbf98e68
luigi-bardi <luigi.bardi1@gmail.com>
2018-03-22 16:15:52 +0100

    Leisure time

--- mywebsite/forms.py
+++ mywebsite/forms.py
@@ -83,7 +83,7 @@ def datep(form, field):
 
 
 class CreateServiceForm(Form):
-    servicetype = SelectField('Type of Service', choices=[('ht', 'Home task'), ('st', 'Shopping task'), ('is', 'Informatic service')])
+    servicetype = SelectField('Type of Service', choices=[('ht', 'Home task'), ('st', 'Shopping task'), ('is', 'Informatic service'), ('lt', 'Leisure time')])
     servicename = StringField("Name of the service", validators=[DataRequired()], render_kw={"placeholder": "Help in doing shopping task"})
     servicedescription = TextAreaField('Service description')
     servicedate=DateField("Date of the service", validators=[DataRequired(),datep], render_kw={"placeholder": "YYYY-MM-DD ('1995-11-19')"})
